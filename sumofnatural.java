7f9bcc672ef5dff584ed42f3f3d8e5eadb18b805
K.Jahnavi Priya <kummaraguntajunnu@gmail.com>
2025-12-14 21:20:58 +0530

    Change upper limit for sum of natural numbers

---
+++ sumofnatural.java
@@ -0,0 +1,10 @@
+public class SumNatural {
+    public static void main(String[] args) {
+        int num = 1000, sum = 0;
+        for(int i = 1; i <= num; ++i)
+        {
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
